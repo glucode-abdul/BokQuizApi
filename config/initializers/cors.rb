944b2735f2060cc31c9ece2e9e7f77a375af8dec
glucode-abdul <abdul01@glucode.com>
2025-10-30 15:58:35 +0200

    CORs patch 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,37 +1,21 @@
-# Configure CORS for React frontend on LAN/dev
-# Configure CORS for React frontend on LAN/dev
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173',
-            'http://localhost:5174',
-            'http://localhost:5175',
-            'http://localhost:5176',
-            'https://bok-quiz-front.vercel.app/',
-            %r{\Ahttp://127\.0\.0\.1:\d+},
-            %r{\Ahttp://192\.168\.\d+\.\d+(?::\d+)?},
-            %r{\Ahttp://10\.\d+\.\d+\.\d+(?::\d+)?},
-            %r{\Ahttp://.*\.local(?::\d+)?},
-            'https://cultural-olympe-abdul-glucode-ffca2586.koyeb.app/'
-    resource '/api/*', headers: :any, methods: %i[get post options]
-    resource '/cable', headers: :any, methods: %i[get post options]
+    origins(
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://localhost:5175',
+      'http://localhost:5176',
+      'https://bok-quiz-front.vercel.app', # ❌ no trailing slash
+      %r{\Ahttp://127\.0\.0\.1:\d+},
+      %r{\Ahttp://192\.168\.\d+\.\d+(?::\d+)?},
+      %r{\Ahttp://10\.\d+\.\d+\.\d+(?::\d+)?},
+      %r{\Ahttp://.*\.local(?::\d+)?},
+      'https://cultural-olympe-abdul-glucode-ffca2586.koyeb.app' # ❌ no trailing slash
+    )
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
-
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
-
-### Remove duplicate default block
